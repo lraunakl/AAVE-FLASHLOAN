8e11e2c93d41ccbffca03b6de2a5ecdd7d338294
raunak.dhabekar <raunak.dhabekar@indicchain.com>
2022-11-03 14:31:22 +0530

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.10",
+  networks: {
+    goerli: {
+      url: process.env.INFURA_GOERLI_ENDPOINT,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
